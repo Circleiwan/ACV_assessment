6ae40d914699db17339557b993ed78df5a527d89
Achmad Setiawan <achmadsetiawan1616@gmail.com>
2021-04-29 14:45:00 +0700

    Updating ANN.py Script

--- ANN.py
+++ ANN.py
@@ -1,25 +1,34 @@
+#!/usr/bin/env python3
+
 import tensorflow as tf
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
+#from sklearn.model_selection import train_test_split
 import numpy as np
 
 class myCallback(tf.keras.callbacks.Callback):
 	def on_epoch_end(self, epoch, logs={}):
 		if logs.get('acc') > 90:
-			print('90 percent has been reached!')
+			print('Accuracy mencapai 90%!')
 			self.model.stop_training=True
 
-df = pd.read_csv('data.csv')
+df = pd.read_csv('merged_data.csv')
+
 properties = list(df.columns.values)
 properties.remove('Catagory')
 X = df[properties]
 X = np.asarray(X).astype('float32')
 y = df['Catagory'] #1.baik; 2.buruk
 
+split = -20
+X_train = X[:split]
+y_train = y[:split]
+X_val = X[split:]
+y_val = y[split:]
+'''
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
-X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=1)
-
+#X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=1)
+'''
 callbacks = myCallback()
 model = tf.keras.Sequential([
 	tf.keras.layers.Flatten(input_shape=(5, )),
@@ -30,7 +39,7 @@ model = tf.keras.Sequential([
 	])
 
 model.compile(optimizer='adam',
-		  loss='mse',
+		  loss='mse', #atau RMSprop
 		  metrics=['acc'])
 
 history = model.fit(X_train, y_train, epochs=34, batch_size=5, validation_data=(X_val, y_val), callbacks=[callbacks])
