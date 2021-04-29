ec08ba12306d18164a14376c49bd8614c6aa3262
Achmad Setiawan <achmadsetiawan1616@gmail.com>
2021-04-29 14:47:35 +0700

    Adding merge_csv.py script

--- merge_csv.py
+++ merge_csv.py
@@ -14,9 +14,5 @@ data1 = pd.read_csv(file_csv[0])
 data2 = pd.read_csv(file_csv[1])
 print(data1)
 print(data2)
-
 output = data1.merge(data2,	on= 'index', how = 'outer')
-with open('merged_data.csv', 'w') as merged_file:
-	writer = csv.writer(merged_file)
-	writer.writerow(output)
-
+output.to_csv('merged_data.csv', index=False)
