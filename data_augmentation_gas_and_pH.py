fbefc958bba0a09f366d2022245adb7acd1a384b
Achmad Setiawan <achmadsetiawan1616@gmail.com>
2021-04-29 14:46:48 +0700

    Cleaning the script and adding an index

--- data_augmentation_gas_and_pH.py
+++ data_augmentation_gas_and_pH.py
@@ -2,31 +2,31 @@ import numpy as np
 import random
 import csv
 
-def data_augmentation_gas(lower, upper, amount):
-  random_list_gas = []
+def data_augmentation(lower, upper, amount):
+  random_list = []
   for _ in range(0,amount):
     number = np.random.uniform(lower,upper)
-    random_list_gas.append(round(number, 2))
-  return random_list_gas
-  
+    random_list.append(round(number, 2))
+  return random_list
+'''
 def data_augmentation_pH(lower, upper, amount):
   random_list_pH = []
   for _ in range(0,amount):
     number = np.random.uniform(lower,upper)
     random_list_pH.append(round(number, 2))
   return random_list_pH
-
+'''
 def generate_csv_file(filename, list_data1, list_data2, name1, name2):
   with open(filename + '.csv', 'w') as f:
     writer = csv.writer(f, lineterminator='\n')
-    writer.writerow([name1,name2])
-    for data in list(zip(list_data1, list_data2)):
-      writer.writerow([data[0], data[1]])
+    writer.writerow(['index',name1,name2])
+    for index, data in enumerate(list(zip(list_data1, list_data2))):
+      writer.writerow([index, data[0], data[1]])
 
 def main():
-  pH_augmentation = data_augmentation_pH(2.5,3.8,100)
+  pH_augmentation = data_augmentation(2.5,3.8,100)
   pH_augmentation.sort(reverse=True)
-  gas_augmentation = data_augmentation_gas(6.0, 8.0,100)
+  gas_augmentation = data_augmentation(6.0, 8.0,100)
   gas_augmentation.sort(reverse=True)
   print('List of pH Augmentation\n',pH_augmentation)
   print('Amount = ',len(pH_augmentation))
